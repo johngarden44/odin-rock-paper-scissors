7de5bcb2facd80907aa4611fd4d996f0c8567d28
John <35427031+johngarden44@users.noreply.github.com>
2023-04-27 19:48:43 -0400

    edit getComputerChoice() to return Rock/Paper/Scissor

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
-function getRandomIntInclusive(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    console.log(min);
-    console.log(max);
-    return Math.floor(Math.random() * (max - min + 1) + min);
+//getComputerChoice returns the computer's randomized selction of 
+//"Rock", "Paper", or "Scissors"
+//I realize that I could just use Math.random as a standalone, 
+//but I was curious how a getRandomIntInclusive type of function worked
+
+function getComputerChoice() {
+    let min = 1;
+    let max = 3;
+    switch (Math.floor(Math.random() * 3 + 1)) {
+    case 1:
+        return "Rock";
+    case 2:
+        return "Paper";
+    case 3:
+        return "Scissors";
+    }
 }
 
-//I realize that I could just use Math.random as a standalone, 
-//but I was curious how a getRandomIntInclusive function worked
-
-console.log(getRandomIntInclusive(1,3))
+console.log(getComputerChoice());
