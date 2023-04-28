511cea2af001c4065a430ef64912cca68f23aae3
John-MB <35427031+johngarden44@users.noreply.github.com>
2023-04-27 23:49:03 -0400

    add scorekeeping variables and loop for 5 round game, plus declaration of winner of 5 rounds

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 let computerChoice;
 let playerChoice;
+let gameCount = 0;
+let playerScore = 0;
+let computerScore = 0;
+
 
 //getComputerChoice returns the computer's randomized selction of 
 //"Rock", "Paper", or "Scissors"
@@ -36,8 +40,10 @@ function game(computerChoice, playerChoice) {
     if (computerChoice === 'rock') {
         switch (playerChoice) {
             case 'scissors':
+                computerScore++;
                 return 'rock beats scissors, computer wins';
             case 'paper':
+                playerScore++;
                 return 'paper beats rock, player wins';
             case 'rock':
                 return 'draw';
@@ -46,10 +52,12 @@ function game(computerChoice, playerChoice) {
     else if (computerChoice === 'paper') {
         switch (playerChoice) {
             case 'scissors':
+                playerScore++;
                 return 'scissors beats paper, player wins';
             case 'paper':
                 return 'draw';
             case 'rock':
+                computerScore++;
                 return 'paper beats rock, computer wins';
         }
     }
@@ -58,12 +66,16 @@ function game(computerChoice, playerChoice) {
             case 'scissors':
                 return 'draw';
             case 'paper':
+                computerScore++;
                 return 'scissors beats paper, computer wins';
             case 'rock':
+                playerScore++;
                 return 'rock beats scissors, player wins';
         }
     }
 }
+
+while (gameCount<5) {
 
 //runs functions to determine player and computer selections
 
@@ -75,3 +87,16 @@ getPlayerChoice();
 console.log(`computer picks ${computerChoice}`);
 console.log(`player picks ${playerChoice}`);
 console.log(game(computerChoice, playerChoice));
+console.log('');
+gameCount++;
+}
+
+//delcares a winner after 5 rounds
+
+if (playerScore>computerScore) {
+    console.log('You won the best of 5 rounds');
+}
+else if (playersScore<computerScore) {
+    console.log('You lost to a computer, how sad');
+}
+else console.log('After 5 rounds, it was a tie');
