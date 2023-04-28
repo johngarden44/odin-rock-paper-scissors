f3610fc8fcea51a89e67e287b0854a16004cf24d
John-MB <35427031+johngarden44@users.noreply.github.com>
2023-04-27 23:32:24 -0400

    add game() and tweak variable names

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let computerChoice;
+let playerChoice;
+
 //getComputerChoice returns the computer's randomized selction of 
 //"Rock", "Paper", or "Scissors"
 //I realize that I could just use Math.random as a standalone, 
@@ -8,11 +11,14 @@ function getComputerChoice() {
     let max = 3;
     switch (Math.floor(Math.random() * 3 + 1)) {
     case 1:
-        return "rock";
+        computerChoice = 'rock';
+        return 'rock';
     case 2:
-        return "paper";
+        computerChoice =  'paper'
+        return 'paper';
     case 3:
-        return "scissors";
+        computerChoice = 'scissors';
+        return 'scissors';
     }
 }
 
@@ -20,8 +26,52 @@ function getComputerChoice() {
 //and returns the entered value (will be updated to verify input)
 
 function getPlayerChoice() {
-    let playerChoice =  prompt("Enter 'rock', 'paper', or 'scissors'");
-        return playerChoice.toLowerCase();
+    playerChoice = prompt("Enter 'rock', 'paper', or 'scissors'").toLowerCase();
+    return playerChoice;
     }
 
-console.log(getPlayerChoice());
+//game compares computerChoice to playerChoice and returns a string declaring a winner
+
+function game(computerChoice, playerChoice) {
+    if (computerChoice === 'rock') {
+        switch (playerChoice) {
+            case 'scissors':
+                return 'rock beats scissors, computer wins';
+            case 'paper':
+                return 'paper beats rock, player wins';
+            case 'rock':
+                return 'draw';
+        }  
+    }
+    else if (computerChoice === 'paper') {
+        switch (playerChoice) {
+            case 'scissors':
+                return 'scissors beats paper, player wins';
+            case 'paper':
+                return 'draw';
+            case 'rock':
+                return 'paper beats rock, computer wins';
+        }
+    }
+    else if (computerChoice === 'scissors') {
+        switch (playerChoice) {
+            case 'scissors':
+                return 'draw';
+            case 'paper':
+                return 'scissors beats paper, computer wins';
+            case 'rock':
+                return 'rock beats scissors, player wins';
+        }
+    }
+}
+
+//runs functions to determine player and computer selections
+
+getComputerChoice();
+getPlayerChoice();
+
+//outputs to screen the player and computer selections and declares winner
+
+console.log(`computer picks ${computerChoice}`);
+console.log(`player picks ${playerChoice}`);
+console.log(game(computerChoice, playerChoice));
